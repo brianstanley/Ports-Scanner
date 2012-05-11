ebe5afe33b9ea007152a37eb40efe772b7049b91
Brian <brianelblog@gmail.com>
2012-05-11 18:55:17 -0300

    Documento un par de cosas

--- scan.pl
+++ scan.pl
@@ -4,16 +4,16 @@ my($host,$port, $data, $protocol); #declaramos variables a usar
 print("Ingresar ip que quiere escanear>"); 
 $host=<STDIN>;
 chomp($host);
-$port=20; #Empieza a escanear desde el puerto 20
+$port=20; #Empiezo a escanear desde el puerto 20
 $protocol = getprotobyname('tcp'); #retorna datos que utilizara el socket de acuerdo a tcp
 for($port;$port<=1000;$port++){
 	socket(Sock_Manejador, PF_INET, SOCK_STREAM, $protocol) || die "socket: $!"; #gestionamos socket
-	$data = sockaddr_in($port,inet_aton($host));#preparamos la data que usaremos para la conexion, el inet atoma traduce en 4 bytes la direccion ip obligatoria para el connect sock
-	if(connect(Sock_Manejador,$data)){ ##conectamos
-	   use Term::ANSIColor; #para colorear en verde si se conecta
+	$data = sockaddr_in($port,inet_aton($host));#preparamos la data que usaremos para la conexion
+	if(connect(Sock_Manejador,$data)){
+	   use Term::ANSIColor; #Para colorear en verde si se conecta
 	   print color 'bold green';
 	   print("El puerto ".$port." esta abierto\n");		
-	   print color 'reset';	# reiniciamos el color por si va a un puerto cerrado para que no aparesca verde
+	   print color 'reset';	
 	}else{
            print "Puerto ".$port." cerrado\n";
 	};
