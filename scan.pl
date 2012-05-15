57f27672999d60b3d6b9e642e03d8285287d251b
Brian <brianelblog@gmail.com>
2012-05-15 13:07:16 -0300

    Documento

--- scan.pl
+++ scan.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 use IO::Socket;
-$host = $ARGV[0];
-$arg = $ARGV[1];
-if($arg =~ /-/){
-  @expl = split('-',$arg);
-  my $cantArg = scalar(@expl);
-  if($cantArg==2){
+$host = $ARGV[0]; # El primer argumento es el host
+$port = $ARGV[1]; # segundo argumento el/los puertos a escanear
+if($port =~ /-/){ # Si se escribe 20-30 se escaneara del puerto 20 AL 30
+  @expl = split('-',$port);
+  my $cantArg = scalar(@expl);# Cuento la cantidad de elementos ingresados en el segundo argumento 
+  if($cantArg==2){ # Si ingreso por ejemplo dos elementos (20-32) se escanea del 20 al 32
     for($port=@expl[0]; $port<=@expl[1];$port++){
             my $con = IO::Socket::INET->new(
                               PeerAddr => $host,
@@ -22,9 +22,9 @@ if($arg =~ /-/){
             }else{
 		print "Puerto ".$port." esta cerrado \n";  
             }   
-    close $con;					  
+    close $con;	# cierro conexión				  
     }
-  }elsif($cantArg==0){
+  }elsif($cantArg==0){  #Si se coloco "-" solamente entonces se escaneara por defecto del puerto 20 al mil
     for($port=20; $port<=1000;$port++){
             my $con = IO::Socket::INET->new(
                               PeerAddr => $host,
@@ -43,27 +43,10 @@ if($arg =~ /-/){
             }  
     close $con;					  
     }
-  }elsif($cantArg==1){
-    my $con = IO::Socket::INET->new(
-                              PeerAddr => $host,
-                              PeerPort => @expl[0],
-                              Proto => 'tcp',
-                              Timeout =>2
-                              );
-              if($con){
-		use Term::ANSIColor; #para colorear en verde si se conecta
-		print color 'bold green';
-		print("El puerto ".$port." esta abierto\n ");
-		print color 'reset';
-		
-            }else{
-		print "Puerto ".$port." esta cerrado \n";  
-            }
-       close $con;					   
   }
-}elsif($arg =~ /,/){
-  @expl = split(',',$arg);
-  my $cantArg = scalar(@expl);
+}elsif($port =~ /,/){ # Si el argumento se separa por coma se escaneara puertos individuales
+  @expl = split(',',$port);
+  my $cantArg = scalar(@expl); #contamos cuantos puertos puso
   for($i=0;$i<$cantArg;$i++){
         my $con = IO::Socket::INET->new(
                               PeerAddr => $host,
@@ -84,17 +67,17 @@ if($arg =~ /-/){
 }else{
         my $con = IO::Socket::INET->new(
                               PeerAddr => $host,
-                              PeerPort => $arg,
+                              PeerPort => $port,
                               Proto => 'tcp',
                               Timeout =>2
                               );
             if($con){
 	      use Term::ANSIColor;
 	      print color 'bold green';	    
-              print("El puerto ".$arg." esta abierto\n");		
+              print("El puerto ".$port." esta abierto\n");		
 	      print color "reset";
 	    }else{
-              print "Puerto ".$arg." esta cerrado \n";  
+              print "Puerto ".$port." esta cerrado \n";  
             }
        close $con;					   
   }	
