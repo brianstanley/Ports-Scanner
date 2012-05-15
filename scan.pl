38365dc51e5f4915e8cbbf20b462a5a223e4a749
Brian <brianelblog@gmail.com>
2012-05-15 12:56:22 -0300

    Paso a manejar sockets pero orientado a objetos

--- scan.pl
+++ scan.pl
@@ -1,22 +1,101 @@
-#/usr/bin/perl-w
-use Socket; #llamamos al modulo socket
-my($host,$port, $data, $protocol); #declaramos variables a usar
-print("Ingresar ip que quiere escanear>"); 
-$host=<STDIN>;
-chomp($host);
-$port=20; #Empiezo a escanear desde el puerto 20
-$protocol = getprotobyname('tcp'); #retorna datos que utilizara el socket de acuerdo a tcp
-for($port;$port<=1000;$port++){
-	socket(Sock_Manejador, PF_INET, SOCK_STREAM, $protocol) || die "socket: $!"; #gestionamos socket
-	$data = sockaddr_in($port,inet_aton($host));#preparamos la data que usaremos para la conexion
-	if(connect(Sock_Manejador,$data)){
-	   use Term::ANSIColor; #Para colorear en verde si se conecta
-	   print color 'bold green';
-	   print("El puerto ".$port." esta abierto\n");		
-	   print color 'reset';	
-	}else{
-           print "Puerto ".$port." cerrado\n";
-	};
-	close(Sock_Manejador);	 # cerramos sockets
-};
-exit;
+#!/usr/bin/perl
+use IO::Socket;
+$host = $ARGV[0];
+$arg = $ARGV[1];
+if($arg =~ /-/){
+  @expl = split('-',$arg);
+  my $cantArg = scalar(@expl);
+  if($cantArg==2){
+    for($port=@expl[0]; $port<=@expl[1];$port++){
+            my $con = IO::Socket::INET->new(
+                              PeerAddr => $host,
+                              PeerPort => $port,
+                              Proto => 'tcp',
+                              Timeout =>2
+                              );
+            if($con){
+		use Term::ANSIColor; #para colorear en verde si se conecta
+		print color 'bold green';
+		print("El puerto ".$port." esta abierto\n ");
+		print color 'reset';
+		
+            }else{
+		print "Puerto ".$port." esta cerrado \n";  
+            }   
+    close $con;					  
+    }
+  }elsif($cantArg==0){
+    for($port=20; $port<=1000;$port++){
+            my $con = IO::Socket::INET->new(
+                              PeerAddr => $host,
+                              PeerPort => $port,
+                              Proto => 'tcp',
+                              Timeout =>2
+                              );
+         if($con){
+		use Term::ANSIColor; #para colorear en verde si se conecta
+		print color 'bold green';
+		print("El puerto ".$port." esta abierto\n ");
+		print color 'reset';
+		
+            }else{
+		print "Puerto ".$port." esta cerrado \n";  
+            }  
+    close $con;					  
+    }
+  }elsif($cantArg==1){
+    my $con = IO::Socket::INET->new(
+                              PeerAddr => $host,
+                              PeerPort => @expl[0],
+                              Proto => 'tcp',
+                              Timeout =>2
+                              );
+              if($con){
+		use Term::ANSIColor; #para colorear en verde si se conecta
+		print color 'bold green';
+		print("El puerto ".$port." esta abierto\n ");
+		print color 'reset';
+		
+            }else{
+		print "Puerto ".$port." esta cerrado \n";  
+            }
+       close $con;					   
+  }
+}elsif($arg =~ /,/){
+  @expl = split(',',$arg);
+  my $cantArg = scalar(@expl);
+  for($i=0;$i<$cantArg;$i++){
+        my $con = IO::Socket::INET->new(
+                              PeerAddr => $host,
+                              PeerPort => @expl[$i],
+                              Proto => 'tcp',
+                              Timeout =>2
+                              );
+            if($con){
+	      use Term::ANSIColor;
+	      print color 'bold green';	    
+              print("El puerto ".@expl[$i]." esta abierto\n");		
+	      print color "reset";
+	    }else{
+              print "Puerto ".@expl[$i]." esta cerrado \n";  
+            }
+       close $con;					   
+  }  
+}else{
+        my $con = IO::Socket::INET->new(
+                              PeerAddr => $host,
+                              PeerPort => $arg,
+                              Proto => 'tcp',
+                              Timeout =>2
+                              );
+            if($con){
+	      use Term::ANSIColor;
+	      print color 'bold green';	    
+              print("El puerto ".$arg." esta abierto\n");		
+	      print color "reset";
+	    }else{
+              print "Puerto ".$arg." esta cerrado \n";  
+            }
+       close $con;					   
+  }	
+	 	 
